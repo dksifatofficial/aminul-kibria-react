073ca61ee9d1993ca4373dbd4bcf3dfa1a2bcc4a
Aminul Kibria <aminulkibria@gmail.com>
2023-06-10 16:01:44 +0600

    res commit

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -8,6 +8,7 @@ import Client from "./Client";
 
 export function Client1() {
   return (
+    <div className={classes.container}>
     <div className={classes.comment}>
       <FontAwesomeIcon className={classes.icon} icon={faQuoteLeft} />
       <p>
@@ -23,6 +24,7 @@ export function Client1() {
           <h6>CEO of Organic Food</h6>
         </Client>
       </div>
+    </div>
     </div>
   );
 }
